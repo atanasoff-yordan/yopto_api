e0807d3f4dbf6ab973d170339a6beb87692d2215
Yordan Atanasoff <atanasoff.yordan@gmail.com>
2021-08-07 00:52:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="yotpo-api",
-    version="0.0.3",
+    version="0.0.4",
     author="yordan atanasoff",
     author_email="atanasoff.yordan@gmail.com",
     description="yotpo api integration",
